4d050d566e357970ad861ff2d4e58b37989ee184
mingyunLEE <leemingyun@naver.com>
2023-07-05 16:34:11 +0900

    Add Server

---
+++ src/main/java/nom/youcanwell/YoucanwellApplication.java
@@ -0,0 +1,13 @@
+package nom.youcanwell;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YoucanwellApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YoucanwellApplication.class, args);
+	}
+
+}
---
+++ src/test/java/nom/youcanwell/YoucanwellApplicationTests.java
@@ -0,0 +1,13 @@
+package nom.youcanwell;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YoucanwellApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
